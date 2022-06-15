ba0a47c35ff0c5ba09a1f2cdf7ff514b3e7df22b
StevenLiu <stevenliu986@gmail.com>
2022-06-15 23:32:24 +1000

    feat: cc-0044-rename folder name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CourtSpecModule } from './modules/court_spec/court_spec.module';
+import { CourtSpecModule } from './resources/court_spec/court_spec.module';
 
 @Module({
   imports: [CourtSpecModule, MongooseModule.forRoot('mongodb://localhost/courtcanvas')],
