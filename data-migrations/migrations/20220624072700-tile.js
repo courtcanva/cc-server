b2703a6ee58db8b2abea3a6d124c087e2d6f1a56
Eason <defend.sen@gmail.com>
2022-06-24 18:15:06 +1000

    chore: delete common

--- data-migrations/migrations/20220624072700-tile.js
+++ data-migrations/migrations/20220624072700-tile.js
@@ -33,9 +33,6 @@ module.exports = {
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
     await db;
     await db.collection("tiles").updateOne(
       { name: "Elite X II" },
