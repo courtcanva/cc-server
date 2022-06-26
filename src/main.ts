9a2b2ccf9cd13a1e89873f2e2da064bf8db9b54d
Jzeva <jzeva1213@gmail.com>
2022-06-26 23:30:56 +1000

    feat: cc-0043 add versioning for RESTful API

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix("v1");
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
