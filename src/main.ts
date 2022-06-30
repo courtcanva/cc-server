ee928c8beb0ff5061f614721d2d488c28debdb5a
Jzeva <50484579+Jzeva@users.noreply.github.com>
2022-06-30 20:09:42 +1000

    fix: fix proxy issue (#18)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.set("trust proxy", 1);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix("v1");
   await app.listen(process.env.PORT || 8080);
