d6ac7206a51126bd72a10c69c4ba939dad73436d
zhuocun <zhuocun.tang@outlook.com>
2022-10-02 22:42:40 +1100

    fix: add more decorators to ensure integrity of parameters of requests

--- src/users/dto/connectAccount.dto.ts
+++ src/users/dto/connectAccount.dto.ts
@@ -1,11 +1,14 @@
-import { IsString } from "class-validator";
+import { IsEmpty, IsNotEmpty, IsString } from "class-validator";
 
 export class ConnectAccountDto {
   @IsString()
+  @IsNotEmpty()
   readonly googleId: string;
 
   @IsString()
+  @IsNotEmpty()
   readonly email: string;
 
+  @IsEmpty()
   readonly updatedAt: Date;
 }
