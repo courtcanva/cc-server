f904fa97180f268a652ab53cceb643042ba3c087
Tianyi Shen <tianyishen97@foxmail.com>
2022-10-07 02:26:40 +1000

    feat: add crud funcs and schema

--- src/template_items/templateItem.service.ts
+++ src/template_items/templateItem.service.ts
@@ -7,9 +7,36 @@ import { TemplateDocument, TemplateItem } from "./schemas/template.schema";
 @Injectable()
 export class TemplateItemService {
   constructor(
-    @InjectModel(TemplateItem.name) private readonly cartItemModel: Model<TemplateDocument>,
+    @InjectModel(TemplateItem.name) private readonly TemplateModel: Model<TemplateDocument>,
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
   // funcs here
+  //   getAll() {
+  //     return;
+  //   }
+
+  //   getAllUserTemplate() {
+  //     return;
+  //   }
+
+  //   findOne() {
+  //     return;
+  //   }
+
+  //   createTemplate() {
+  //     return;
+  //   }
+
+  //   removeTemplateByUser() {
+  //     return;
+  //   }
+
+  //   undateTemplateByAdmin() {
+  //     return;
+  //   }
+
+  //   deleteTemplate() {
+  //     return;
+  //   }
 }
