82ae1813e7f9882608f5971de23658a313bf3c4f
Tianyi Shen <tianyishen97@foxmail.com>
2022-10-07 16:58:39 +1000

    feat: update fto

--- src/template_items/dto/template.dto.ts
+++ src/template_items/dto/template.dto.ts
@@ -1,26 +1,105 @@
 import { Type } from "class-transformer";
-import { IsNotEmpty, IsObject, IsString, MaxLength, ValidateNested } from "class-validator";
-import { Design } from "src/cart_items/dto/create-cartItem.dto";
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from "class-validator";
+
+export class Color {
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  color: string;
+}
+
+export class CourtSize {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  length: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  width: number;
+
+  @IsNumber()
+  centreCircleRadius: number;
+
+  @IsNumber()
+  threePointRadius: number;
+
+  @IsNumber()
+  threePointLine: number;
+
+  @IsNumber()
+  lengthOfCorner: number;
+
+  @IsNumber()
+  restrictedAreaLength: number;
+
+  @IsNumber()
+  restrictedAreaWidth: number;
+
+  @IsNumber()
+  sideBorderWidth: number;
+
+  @IsNumber()
+  lineBorderWidth: number;
+}
+
+export class Design {
+  @IsString()
+  @IsOptional()
+  readonly designer?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly designName: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => Color)
+  readonly tileColor: Color[];
+
+  @IsObject()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CourtSize)
+  readonly courtSize: CourtSize;
+}
 
 export class TemplateItemDto {
   @IsString()
   @IsNotEmpty()
   readonly userId: string;
-  // 需要readonly吗？
 
   @IsString()
   @IsNotEmpty()
-  image: string;
+  readonly image: string;
 
   @IsObject()
   @IsNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => Design)
-  design: Design;
+  readonly design: Design;
 
   @IsString()
   @MaxLength(200)
-  description: string;
+  readonly description: string;
 
   // automaticly generate the tags??? or manual input？
 
