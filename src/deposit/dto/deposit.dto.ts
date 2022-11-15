fed4642aa2a31e76182f8319aefd3308d15d26ec
Tianyi Shen <tianyishen97@foxmail.com>
2022-11-15 20:16:41 +1000

    feat: optimise update dto

--- src/deposit/dto/deposit.dto.ts
+++ src/deposit/dto/deposit.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsPositive } from "class-validator";
+import { IsNotEmpty, IsPositive, Max } from "class-validator";
 
 export class DepositDto {
   @IsNotEmpty()
   @IsPositive()
+  @Max(1)
   readonly depositRatio: number;
 }
