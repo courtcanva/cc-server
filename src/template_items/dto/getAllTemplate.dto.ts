dbbec99051f5dc1489868472488d7929631ae7b1
Zack Chen <zackchen5813@gmail.com>
2022-11-16 23:02:12 +1100

    fix: remove non-used  import

--- src/template_items/dto/getAllTemplate.dto.ts
+++ src/template_items/dto/getAllTemplate.dto.ts
@@ -1,4 +1,4 @@
-import { isEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 export class GetAllTemplatesDto {
   @IsString()
   @IsNotEmpty()
