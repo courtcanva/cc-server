40287736da13cf056b2daf5f8ff7f4dd70ce06b4
shuzheng wang <shawn.wang86b@gmail.com>
2022-11-27 20:41:21 +1100

    fix: admin page function

--- src/auth/types/tokens.type.ts
+++ src/auth/types/tokens.type.ts
@@ -2,3 +2,14 @@ export type Tokens = {
   accessToken: string;
   refreshToken: string;
 };
+
+export interface IAdmin {
+  id: string;
+  email: string;
+  name: string;
+  permission: string;
+}
+
+export interface IadminLoginReponseData extends Tokens {
+  admin: IAdmin;
+}
